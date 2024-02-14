cb9eb9b62a6c3edeaad79de30e7213898dba32b8
Derek Medrano <derekjmedrano@gmail.com>
2024-02-14 16:42:53 -0600

    started index.js file for inquirer prompts

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '!Weep&UWeepAlone19992009',
+        database: 'employees_db'
+    }
+)
+
+
